e6e76ac8f28189ea82f42ce47d6730bdeaf833f4
mininakar <minyailo.karina@gmail.com>
2022-04-02 15:43:31 +0500

    part 3

--- Source.cpp
+++ Source.cpp
@@ -21,7 +21,7 @@ void RenderSceneCB() //функция обратного вызова т.к мы работаем в оконной систем
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(0);
 
 	glutSwapBuffers();//прорисовываем буфер на экран
@@ -49,21 +49,17 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	glm::vec3 Vertices[1];
+	glm::vec3 Vertices[3];
 	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
-
+	Vertices[1] = glm::vec3(5.0f, 0.0f, 0.0f);
+	Vertices[2] = glm::vec3(1.0f, -1.0f, 0.0f);
 	
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 	glutMainLoop();// Основной цикл GLUT
 
-	//glEnableVertexAttribArray(0);
-
-	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	//glDrawArrays(GL_POINTS, 0, 1);
-	//glDisableVertexAttribArray(0);
+	
 
 	return 1;
 }
