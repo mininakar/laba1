f3bd05b6f60a8711c60da7b902edc4e646c78bd5
mininakar <minyailo.karina@gmail.com>
2022-04-02 15:38:39 +0500

    part 2

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,33 @@
 #include	<iostream>
+#include	<GL/glew.h>
 #include	<gl/freeglut.h>
 #include	<gl/glu.h>
 #include	<gl/glut.h>
+#include <glm/glm.hpp>
+#include <glm/vector_relational.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
+
+GLuint VBO;
+//#include	"math_3d.h"
 
 
- void RenderSceneCB() //функция обратного вызова т.к мы работаем в оконной системе
+
+
+void RenderSceneCB() //функция обратного вызова т.к мы работаем в оконной системе
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//очищаем буфер
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 1);
+	glDisableVertexAttribArray(0);
+
+	glutSwapBuffers();//прорисовываем буфер на экран
 }
+
+	
 
 int main(int argc, char** argv) {
 
@@ -17,12 +36,34 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE| GLUT_RGBA);// выбираем режим отображения дисплея RGBA-для цветного вывода
 	glutInitWindowPosition(100, 100); //инициализация верхнего левого угла окна 
 	glutInitWindowSize(1024, 1024);//инициализация размера окна 
-	glutCreateWindow("Lesson #1"); // создаст окно
+	glutCreateWindow("Lesson #2"); // создаст окно
 
 	glutDisplayFunc(RenderSceneCB); //устанавливает обратный вызов дисплея для текущего окна.
 
-	glClearColor(0.0f, 255.0f, 0.0f, 0.0f);
-	glutMainLoop();// Основной цикл GLUT
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	glm::vec3 Vertices[1];
+	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+
 	
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	glutMainLoop();// Основной цикл GLUT
+
+	//glEnableVertexAttribArray(0);
+
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//glDrawArrays(GL_POINTS, 0, 1);
+	//glDisableVertexAttribArray(0);
+
 	return 1;
 }
